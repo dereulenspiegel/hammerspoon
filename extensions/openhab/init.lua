35ee339844ad86df48afc6031faa26554a43e3bc
Till Klocke <till.klocke@gmail.com>
2015-04-26 17:25:39 +0200

    Added documentation for the openhab module

--- extensions/openhab/init.lua
+++ extensions/openhab/init.lua
@@ -12,12 +12,28 @@ function printError(status, data)
 	print("Response: "..data)
 end
 
+--- hs.openhab.new(url) -> table
+--- Function
+--- This creates a new table representing an openHAB server
+---
+--- Parameters:
+---  * url - The URL of the openHAB server
+---
+--- Returns:
+---  * A table representing the server
 function openhab.new(url)
 	s = server
 	s.baseurl = url
 	return s
 end
 
+--- hs.openhab:sendCommand(item, command)
+--- Method
+--- Send a command to an item
+---
+--- Parameters:
+---  * item - The item name as a string
+---  * command - The string representation of an openHAB command
 function server:sendCommand(item, command)
 	headers = {}
 	headers["Content-Type"] = "text/plain"
@@ -28,6 +44,14 @@ function server:sendCommand(item, command)
 	end
 end
 
+--- hs.openab:getState(item) -> string
+--- Method
+--- Retrieves the string representation of the current item state
+---
+--- Parameters:
+---  * item - String containing the item name
+--- Returns:
+---  * The string representation of the item state or nil
 function server:getState(item)
 	status, data, headers = http.get(self.baseurl.."/rest/items/"..item.."/state")
 	if status == 200 then
